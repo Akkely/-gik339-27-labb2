e6a93444aa12c8bcfda1a4312b46809952c3e9d0
Akkely <112615486+Akkely@users.noreply.github.com>
2023-12-12 09:02:44 +0100

    Senaste

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-// test
+const express = require("express");
+const server = express();
+
+server
+	.use(express.json())
+	.use(express.urlencoded({ extended: false }))
+	.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "*");
+		res.header("Access-Control-Allow-Methods", "*");
+		next();
+	});
+
+server.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000 ");
+});
+
+server.get("/users", (req, res) => {
+	const method = req.method;
+	const url = req.url;
+	res.send(`Du gjorde en ${method}-förfrågan till url:en ${url}`);
+});
